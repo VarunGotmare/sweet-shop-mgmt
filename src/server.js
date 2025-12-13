5e077081afdfccf43c07c30f98afdc7117a49d0c
VarunGotmare <varunpgotmare@gmail.com>
2025-12-13 18:18:44 +0530

    feat: escaped the prisma 7 setup hell
    I REQUIRE BETTER DOCS OMG

--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const prisma = require('./lib/prisma');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/health', (req, res) => {  
-    res.json({status: "OK"});
+app.get('/health', async (req, res) => {  
+    try{
+        const users = await prisma.user.findMany();
+        res.json({ status: 'ok', users });
+    }catch(error){
+        res.status(500).json({ status: 'error', message: error.message });
+    }
+    
 });
 
 const PORT = process.env.PORT || 4000; // 3000 for fronedtend ig
